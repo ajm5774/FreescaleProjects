d98d90b4739cb8e0d696756b72f9a631118393ae
Andrew <ajm5774@gmail.com>
2014-12-07 16:44:55 -0500

    fsdf

--- Project6/Sources/main.c
+++ Project6/Sources/main.c
@@ -2,30 +2,72 @@
 #include "derivative.h"      /* derivative-specific definitions */
 
 const int BUTTONID = 5; 
-servo theServo = {0,0,0,0,0};
+servo theServo = { 0, 0, &PWMDTY0, &PWMPER0, &PORTB };
+byte inputByte = 0;
+int inputIndex = 0;
+int hasInput = 0;
 
-void main(void) {
-  float input;
-  waitForButtonPushed(BUTTONID);
+
+void InitializeTimer(void)
+{
+  TIE_C1I = 0;//make sure the timer is disabled at the start
+
+  // Set the timer prescaler to %2, since the bus clock is at 2 MHz,
+  // and we want the timer running at 1 MHz
+  TSCR2_PR0 = 1;
+  TSCR2_PR1 = 0;
+  TSCR2_PR2 = 0;
+    
+  //Capture input on rising edge only
+  TCTL4_EDG1B = 1;
+  TCTL4_EDG1A = 1;
   
-  if(!ack())
-     theServo->leds &= LED_0_ON;
+  // Enable the timer
+  TSCR1_TEN = 1;
+
+  // Enable interrupts via macro provided by hidef.h
+  EnableInterrupts;
+}
+
+
+//Input capture. Captures the current time, quick calculates the time
+//then increments the occurences of the time in the measurements array
+#pragma push
+#pragma CODE_SEG __SHORT_SEG NON_BANKED
+//--------------------------------------------------------------       
+void interrupt 9 OC1_isr( void )
+{
+  //clear the interrupt flag   
+  TFLG1   =   TFLG1_C1F_MASK;
   
-  while(true) {
-      input = readInput();
-      mov(theServo, input); 
+  inputByte = inputByte << 1 & TC1;
+  
+  if(++inputIndex >= 7) {
+    hasInput = 1;
   }
 }
+#pragma pop
 
-void waitForButtonPushed(int buttonId) {
-  
-  
-}
 
 int ack() {
   
 }
 
-float readInput() {
-    
+
+void main(void) {
+  waitForButtonPushed();
+  
+  if(!ack())
+     theServo->leds &= LED_0_ON;
+  
+  while(true) {
+    if(hasInput) {
+      mov(theServo, inputByte);
+      inputIndex = 0;//reset input index
+      hasInput = 0;  //reset input flag
+    }
+  }
 }
+
+
+
